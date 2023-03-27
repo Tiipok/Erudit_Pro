48e493b18ea2074b168b73a7fb955a0e197ebe7b
Tipok <60702813+Tiipok@users.noreply.github.com>
2023-03-27 22:38:10 +0300

    more convenient

--- gen_three_words.py
+++ gen_three_words.py
@@ -3,13 +3,15 @@ from random import randint
 from referens import alph, numeration
 
 
-def gen_three_words(letter):
+def Gen_Three_Words(letter):
     with open("Dict.csv", 'r', encoding="utf8") as file:
         csvreader = csv.reader(file)
         lines = list(csvreader)
         letter = letter.lower()
+
         if letter == 'ё': letter = 'е'
         if letter not in alph: return f'Я не нашел слов на букву: {letter}'
+
         a = numeration[alph[letter]]
 
         first = randint(a[0], a[1])
@@ -24,5 +26,5 @@ def gen_three_words(letter):
         second = lines[second][0]
         third = lines[third][0]
 
-        return [first, second, third]
+        return ' '.join(x for x in [first, second, third])
 
