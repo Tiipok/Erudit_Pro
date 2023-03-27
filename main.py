b3f2c3f27e31fed30fdea810f85ada3e4f54d706
Tipok <60702813+Tiipok@users.noreply.github.com>
2023-03-27 22:37:16 +0300

    new main

--- main.py
+++ main.py
@@ -1,114 +1,136 @@
 from flask import Flask, request
 from dialogs import *
 from random import randint
-from gen_three_words import gen_three_words
+from TroyChepuha import *
+from Find_Definition import check
+import pymorphy2
 
+morph = pymorphy2.MorphAnalyzer()
 app = Flask(__name__)
 
-in_game = 0  # 0 - no game, 1 4 - troynaya chepuha, 2 5 - zapresh buk, 3 6 - abbrev
-turn = 0  # 0-start   1-game
-
+status = 0
 
 @app.route('/', methods=['POST'])
-def resp():
-    global in_game
-    global turn
+def response():
     end_session = False
-    buttons = []
+    global status
 
     text = request.json.get('request', ()).get('command')
+    text = text.lower()
+
     response_text = ''
+    buttons = []
 
-    if in_game > 3:  # gaming
-        for word in exit_phrases:
-            if word in text:
-                in_game = 0
-                print('exit game')
-
-        if in_game == 4:
-            l = gen_three_words(text)
-            response_text = ' '.join(x for x in l)
-            print(4)
-
-        if in_game == 5:
-            response_text = str(randint(0, 10))
-            print(5)
-
-        if in_game == 6:
-            response_text = str(randint(0, 10))
-            print(6)
-
-    if in_game < 4:
-        if in_game > 0:
-            if in_game == 1 and text in agree:  # entering and exiting games
-                response_text = 'Начинаем! Загадайте букву.'
-                in_game = 4
-            elif in_game == 1 and text in disagree:
-                response_text = 'Хорошо! Закрываю навык "Эрудит".'
-                end_session = True
-
-            elif in_game == 2 and text in agree:
-                response_text = 'Какую букву будем избегать?'
-                in_game = 5
-            elif in_game == 2 and text in disagree:
-                response_text = 'Хорошо! Закрываю навык "Эрудит".'
-                end_session = True
-
-            elif in_game == 3 and text in agree:
-                response_text = 'Начинаем! Загадайте слово.'
-                in_game = 6
-            elif in_game == 3 and text in disagree:
-                response_text = 'Хорошо! Закрываю навык "Эрудит".'
-                end_session = True
-
-            else:
-                in_game = 0
-
-        if in_game == 0:
-            if not text:
-                response_text = intro  # from dialogs
-                buttons = [{'title': 'Правила каждой', 'hide': False},
-                           {'title': 'Тройная чепуха', 'hide': False},
-                           {'title': 'Запрещённая буква', 'hide': False},
-                           {'title': 'Аббревиатура', 'hide': False}, ]
-
-            elif text == 'правила каждой':
-                response_text = rules
-                buttons = [{'title': 'Правила каждой', 'hide': False},
-                           {'title': 'Тройная чепуха', 'hide': False},
-                           {'title': 'Запрещённая буква', 'hide': False},
-                           {'title': 'Аббревиатура', 'hide': False}, ]
-
-            elif text == 'аббревиатура':
-                in_game = 3
-                response_text = abr_rules
-                buttons = [{'title': 'Я согласен', 'hide': False},
-                           {'title': 'Не хочу', 'hide': False}, ]
-
-            elif text == 'запрещённая буква':
-                in_game = 2
-                response_text = zapr_rules
-                buttons = [{'title': 'Я согласен', 'hide': False},
-                           {'title': 'Не хочу', 'hide': False}, ]
-
-            elif text == 'тройная чепуха':
-                in_game = 1
-                response_text = troynaya_rules
-                buttons = [{'title': 'Я согласен', 'hide': False},
-                           {'title': 'Не хочу', 'hide': False}, ]
-            else:
-                response_text = 'Я вас не поняла, повторите пожалуйста.'
-                buttons = [{'title': 'Правила каждой', 'hide': False},
-                           {'title': 'Тройная чепуха', 'hide': False},
-                           {'title': 'Запрещённая буква', 'hide': False},
-                           {'title': 'Аббревиатура', 'hide': False}, ]
-
+    # checking if user wants to exit
     if text in exit_phrases:
-        response_text = 'До свидания!'
+        response_text = 'До свидания! Закрываю навык.'
         end_session = True
         buttons = []
+        status = -1
 
-    response = {
+    # games branch
+    if status in [4, 5, 6]:
+
+        if status == 4:
+            pass
+
+        elif status == 5:
+            pass
+
+        elif status == 6:
+            pass
+
+    # pregame answer
+    if status in [1, 2, 3]:
+
+        if status == 1 and text in agree:
+            response_text = 'Начинаем! Загадайте букву.'
+            status = 4
+            buttons = []
+
+        elif status == 1 and text in disagree:
+            response_text = 'Может в другую игру?'
+            status = 0
+            buttons = [{'title': 'Правила каждой', 'hide': False},
+                       {'title': 'Тройная чепуха', 'hide': False},
+                       {'title': 'Запрещённая буква', 'hide': False},
+                       {'title': 'Аббревиатура', 'hide': False}, ]
+
+
+        elif status == 2 and text in agree:
+            response_text = 'Какую букву будем избегать?'
+            status = 5
+            buttons = []
+
+        elif status == 2 and text in disagree:
+            response_text = 'Может в другую игру?'
+            status = 0
+            buttons = [{'title': 'Правила каждой', 'hide': False},
+                       {'title': 'Тройная чепуха', 'hide': False},
+                       {'title': 'Запрещённая буква', 'hide': False},
+                       {'title': 'Аббревиатура', 'hide': False}, ]
+
+
+        elif status == 3 and text in agree:
+            response_text = 'Начинаем! Загадайте слово.'
+            status = 6
+            buttons = []
+
+        elif status == 3 and text in disagree:
+            response_text = 'Может в другую игру?'
+            status = 0
+            buttons = [{'title': 'Правила каждой', 'hide': False},
+                       {'title': 'Тройная чепуха', 'hide': False},
+                       {'title': 'Запрещённая буква', 'hide': False},
+                       {'title': 'Аббревиатура', 'hide': False}, ]
+
+        else:
+            response_text = 'Прости я тебя не понял. Если хочешь сыграть скажи "Я согласен", если нет то "Не хочу"'
+            buttons = [{'title': 'Я согласен', 'hide': False},
+                       {'title': 'Не хочу', 'hide': False}, ]
+
+    # main branch
+    if status == 0:
+        if not text:
+            response_text = intro  # from dialogs
+            buttons = [{'title': 'Правила каждой', 'hide': False},
+                       {'title': 'Тройная чепуха', 'hide': False},
+                       {'title': 'Запрещённая буква', 'hide': False},
+                       {'title': 'Аббревиатура', 'hide': False}, ]
+
+        elif text == 'правила каждой':
+            response_text = rules
+            buttons = [{'title': 'Правила каждой', 'hide': False},
+                       {'title': 'Тройная чепуха', 'hide': False},
+                       {'title': 'Запрещённая буква', 'hide': False},
+                       {'title': 'Аббревиатура', 'hide': False}, ]
+
+        elif text == 'аббревиатура':
+            status = 1
+            response_text = abr_rules
+            buttons = [{'title': 'Я согласен', 'hide': False},
+                       {'title': 'Не хочу', 'hide': False}, ]
+
+        elif text == 'запрещённая буква':
+            status = 2
+            response_text = zapr_rules
+            buttons = [{'title': 'Я согласен', 'hide': False},
+                       {'title': 'Не хочу', 'hide': False}, ]
+
+        elif text == 'тройная чепуха':
+            status = 3
+            response_text = troynaya_rules
+            buttons = [{'title': 'Я согласен', 'hide': False},
+                       {'title': 'Не хочу', 'hide': False}, ]
+
+        else:
+            response_text = 'Я вас не поняла, повторите пожалуйста.'
+            buttons = [{'title': 'Правила каждой', 'hide': False},
+                       {'title': 'Тройная чепуха', 'hide': False},
+                       {'title': 'Запрещённая буква', 'hide': False},
+                       {'title': 'Аббревиатура', 'hide': False}, ]
+
+    resp = {
         'response': {
             'text': response_text,
             'end_session ': end_session,
@@ -116,12 +138,11 @@ def resp():
         },
         'version': '1.0'
     }
-
     print(request.json)
+    print(status)
     print(text)
-    print('status', in_game)
 
-    return response
+    return resp
 
 
 app.run(host='0.0.0.0', port=5000, debug=True)
