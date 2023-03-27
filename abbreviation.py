6a75761a49f84e3f8e4dbaa4bbd9e31d7be87954
Tiipok <boolyan90@gmail.com>
2023-03-27 23:44:20 +0300

    bla bla

--- abbreviation.py
+++ abbreviation.py
@@ -1,13 +1,13 @@
 from random import choice
 from referens import alph, numeration #{'a': 1, ...}, [[beg, end], ...]
 import csv
-def make_sentence(in_word):
+def make_sentence(sls):
 
     with open("Dict.csv", 'r', encoding="utf8") as file:
         csvreader = csv.reader(file)
         lines = list(csvreader)
 
-    sls, ans = list(in_word), []
+    ans = []
     for i in range(len(sls)): #reconstruction
         letter = sls[i]
         letter = letter.lower()
@@ -15,21 +15,16 @@ def make_sentence(in_word):
         if letter not in alph: return f'Я не нашел слов на букву: {letter}'
         a = numeration[alph[letter]]
 
-        def filter_word(word):
-            new_word = ''
-            for s in word:
-                if s.isalpha():
-                    new_word += s
-
-            return new_word
-
-        out_word = choice(lines[a[0]: a[1]])[0]
+        out_word = choice(lines[a[0] : a[1]])[0]
         if out_word in ans:
             limit = 0
             while out_word in ans:
-                out_word = choice(lines[a[0]: a[1]])[0]
+                out_word = choice(lines[a[0] : a[1]])[0]
                 limit += 1
                 if limit == 1000: return 'Слишком много символов'
-        ans.append(filter_word(out_word).strip())
+
+        for k in ('!', '?', '.'): #new filter
+            out_word = out_word.strip(k)
+        ans.append(out_word.strip())
 
     return ' '.join(ans)
